38ab3813ae1692d73a6d36a48eb098d3b2baf369
bjerkek <bjerkek@gmail.com>
2020-11-05 12:11:52 +0100

    Initial commit service

---
+++ sb1-secret-service/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+  const myAPIKey = 'process.env.MYAPIKEY';
+  res.send('Hello from SpareBank 1 Hackathon from ' + process.env.HACKATHON_SECRET);
+});
+
+app.listen(8080, () => {
+  console.log(`Server listening on port 8080...`);
+});
